3052a2260162a30f8c8becb9234e243242f8a2ad
ettienekorayyi <corralstephenmelben@gmail.com>
2020-12-17 14:11:21 +1100

    Created Expense Tracker project files.

---
+++ ExpenseTrackerTests/Controllers/ControllerTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Controllers
+{
+    public class ControllerTests
+    {
+       [Fact]
+       public void Get()
+       {
+           // Enter your unit test code here
+       }
+    }
+}
