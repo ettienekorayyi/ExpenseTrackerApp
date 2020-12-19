dbc97e07351486202fea6acab3bf377a3a3c11be
ettienekorayyi <corralstephenmelben@gmail.com>
2020-12-19 23:39:07 +1100

    Created ExpensesController API with CRUD features.

--- ExpenseTracker/API/Controllers/ExpensesController.cs
+++ ExpenseTracker/API/Controllers/ExpensesController.cs
@@ -1,43 +1,101 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
 using Microsoft.AspNetCore.Mvc;
+using Persistence;
 
 namespace ExpenseTracker.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ExpensesController
+    public class ExpensesController : ControllerBase
     {
+        private readonly ExpenseTrackerDbContext _dbContext;
+        public ExpensesController(ExpenseTrackerDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
         // GET api/expenses
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public ActionResult<IEnumerable<Expense>> Get()
         {
-            return new string[] { "test1", "test2" };
+            var results = _dbContext.Expenses.AsQueryable();
+
+            if (results == null)
+                return NotFound();
+
+            return Ok(results);
         }
 
         // GET api/expenses/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public ActionResult<Expense> Get(Guid id)
         {
-            return "value";
+            var result = _dbContext.Expenses
+                .FirstOrDefault(exp => exp.Id == id);
+
+            if (result == null)
+                return NotFound();
+
+            return Ok(result);
         }
 
         // POST api/expenses
         [HttpPost]
-        public void Post([FromBody] string value)
+        public ActionResult Post([FromBody] Expense expense)
         {
+            if (expense == null)
+                return BadRequest();
+
+            _dbContext.Add(expense);
+            _dbContext.SaveChanges();
+
+            return Created("/api/expenses/", 
+                new { Result = "New Expense has been added." });            
         }
 
         // PUT api/expenses/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public ActionResult Put(Guid id, [FromBody] Expense expense)
         {
+            if(id != expense.Id)
+                BadRequest();
+
+            var result = _dbContext.Expenses
+                .FirstOrDefault(exp => exp.Id == id);
+
+            if(result == null)
+                NotFound();
+            
+            result.Description = expense.Description;
+            result.Category = expense.Category;
+            result.Shop = expense.Shop;
+            result.Quantity = expense.Quantity;
+            result.Amount = expense.Amount;
+            result.Date = expense.Date;
+
+            _dbContext.SaveChanges();
+
+            return NoContent();
         }
 
         // DELETE api/expenses/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public ActionResult Delete(Guid id)
         {
+            var result = _dbContext.Expenses.FirstOrDefault(exp => exp.Id == id);
+
+            if(result == null)
+                NotFound();
+
+            _dbContext.Remove(result);
+            _dbContext.SaveChanges();
+
+            return NoContent();
         }
-        
+
     }
 }
