9bc56f2cf16e0fcb3ce283e48c9be9d93ea7e13c
ettienekorayyi <corralstephenmelben@gmail.com>
2021-01-14 21:53:38 +1100

    Created the CurrentExpenses layout

--- ExpenseTracker/client-app/src/app/layout/App.js
+++ ExpenseTracker/client-app/src/app/layout/App.js
@@ -1,16 +1,17 @@
 import { Grid } from "semantic-ui-react";
 import Header from "../../features/nav/Header";
 import ExpensesList from "../../features/expenses/dashboard/ExpensesList";
-//import RecentExpensesList from "../../features/expenses/dashboard/RecentExpensesList";
+import CurrentExpenses from "../../features/expenses/dashboard/CurrentExpenses";
 import "./styles.css";
 
 function App() {
   return (
-    <Grid columns={3} >
+    <Grid columns={3} id='expenses'>
       <Grid.Column id='navigation-grid'>
        <Header />
       </Grid.Column>
-      <Grid.Column id='expenses-grid'>
+      <Grid.Column >
+        <CurrentExpenses />
         <ExpensesList />
       </Grid.Column>
     </Grid>
